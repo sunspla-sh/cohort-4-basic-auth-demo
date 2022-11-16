e35ae39ab218db505dc23afa8173e972dceb1fb7
jack <jrfisch95@gmail.com>
2022-11-16 10:10:40 -0500

    separate controllers

---
+++ controllers/auth.controllers.js
@@ -0,0 +1,61 @@
+const bcryptjs = require('bcryptjs');
+const User = require('../models/User.model');
+
+
+const signupGetController = (req, res, next) => {
+  res.render('signup.hbs');
+};
+
+const signupPostController = (req, res, next) => {
+  console.log(req.body);
+
+  if(!req.body.email || ! req.body.password){
+    res.send('Sorry you forgot an email or password');
+    return;
+  }
+
+  User.findOne({ email: req.body.email })
+    .then(foundUser => {
+      
+      if(foundUser){
+        res.send('Sorry user already exists');
+        return;
+      }
+
+      const myHashedPassword = bcryptjs.hashSync(req.body.password)
+
+      return User.create({
+        email: req.body.email,
+        password: myHashedPassword
+      })
+      
+    })
+    .then(createdUser => {
+      console.log("here's the new user", createdUser);
+      res.send(createdUser);
+    })
+    .catch(err => {
+      console.log(err);
+      res.send(err);
+    })
+};
+
+const loginGetController = (req, res, next) => {
+  res.render('login.hbs');
+};
+
+
+/**
+ * check if user exists
+ * check if password is the same
+ */
+const loginPostController = (req, res, next) => {
+  res.send('ok');
+}
+
+module.exports = {
+  signupGetController,
+  signupPostController,
+  loginGetController,
+  loginPostController
+};
--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,49 +1,18 @@
-const User = require('../models/User.model');
-const bcryptjs = require('bcryptjs');
+const { 
+  signupGetController,
+  signupPostController,
+  loginGetController,
+  loginPostController
+} = require('../controllers/auth.controllers');
 
 const router = require('express').Router();
 
-router.get('/signup', (req, res, next) => {
-  res.render('signup.hbs');
-});
+router.get('/signup', signupGetController);
 
+router.post('/signup', signupPostController);
 
-router.post('/signup', (req, res, next) => {
-  console.log(req.body);
+router.get('/login', loginGetController);
 
-  if(!req.body.email || ! req.body.password){
-    res.send('Sorry you forgot an email or password');
-    return;
-  }
-
-  User.findOne({ email: req.body.email })
-    .then(foundUser => {
-      
-      if(foundUser){
-        res.send('Sorry user already exists');
-        return;
-      }
-
-      const myHashedPassword = bcryptjs.hashSync(req.body.password)
-
-      return User.create({
-        email: req.body.email,
-        password: myHashedPassword
-      })
-      
-    })
-    .then(createdUser => {
-      console.log("here's the new user", createdUser);
-      res.send(createdUser);
-    })
-    .catch(err => {
-      console.log(err);
-      res.send(err);
-    })
-});
-
-router.get('/login', (req, res, next) => {
-  res.render('login.hbs');
-});
+router.post('/login', loginPostController);
 
 module.exports = router;
